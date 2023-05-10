812dd1a53cdaab6c20d050d5c9de3a55c893d61a
sarah <sallylittlecat@gmail.com>
2023-05-10 14:02:25 -0300

    mudei

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <PrimeiroComponente/>
+    <p>{{ teste }}</p>
+  </div>
+  
 </template>
-
+<!--aq usa o componente ao Importar e utilizar a tag dele -->
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//componente em root da aplicação
+import PrimeiroComponente from "./components/PrimeiroComponente.vue"
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default{
+    name: "app",
+    components: {
+      PrimeiroComponente
+    },
+    data(){
+      return {
+        teste: "testando"
+      }
+    }
   }
-}
-</script>
+  
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+    </div>
+</template>
+ <!--é criado um componente e exportado-->
+<script>
+    export default{
+        nome: "PrimeiroComponente",
+        data(){ //retorno de dados
+            return { //coloca dados no objeto pra ser usado no componente
+                nome: "Matehus", //dado chamado nome
+                profissao: "programador"
+            }
+        }
+    }
+</script>
